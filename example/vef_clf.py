4c25d59ba88e81a2d608cc3e9cbd55f7d7ec5a19
Yujin-nKim <yujinkimvv@gmail.com>
2023-08-07 16:06:39 +0900

    Revert Commit

--- example/vef_clf.py
+++ example/vef_clf.py
@@ -10,17 +10,6 @@ Train and save VEF classifiers
 """
 import argparse
 from vef import VCFDataset, Classifier
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-from sklearn.metrics import roc_curve, auc
-from sklearn.model_selection import train_test_split
-from sklearn.datasets import make_classification
-from sklearn.linear_model import LogisticRegression
-
-
 
 def main():
     parser = argparse.ArgumentParser(
@@ -40,8 +29,8 @@ python vef_clf.py --happy path/to/NA12878.vcf.happy.vcf --target path/to/NA12878
 
     optional = parser.add_argument_group("optional arguments")
     optional.add_argument("-n", "--num_trees", help="number of trees, default = 150", type=int, default=150)
-    optional.add_argument("--kind", choices=["RF", "RandomForest", "AB", "AdaBoost", "GB", "GradientBoost","SVM","SupportVector"], type=str, default="RF",
-            help="kind of ensemble methods, available values: RandomForest (RF), AdaBoost (AB), GradientBoost(GB), SupportVector(SVM); default = RF")
+    optional.add_argument("--kind", choices=["RF", "RandomForest", "AB", "AdaBoost", "GB", "GradientBoost"], type=str, default="RF",
+            help="kind of ensemble methods, available values: RandomForest (RF), AdaBoost (AB), GradientBoost(GB); default = RF")
 
     args = parser.parse_args()
     vcf_hap = args.happy
@@ -52,34 +41,9 @@ python vef_clf.py --happy path/to/NA12878.vcf.happy.vcf --target path/to/NA12878
     dataset = VCFDataset(vcf_hap, vcf_tgt, mode)
     X, y = dataset.get_dataset('*')
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
     clf = Classifier(dataset.features, n_trees, kind)
-    clf.gridsearch(X, y, 5, 2)
-    clf.fit(X_train, y_train)
-
-
-    y_probs = clf.predict_proba(X_test)[:, 1]
-
-    fpr, tpr, thresholds = roc_curve(y_test, y_probs)
-
-    roc_auc = auc(fpr, tpr)
-    
-    
+    clf.fit(X, y)
     clf.save(vcf_tgt + ".vef_{}_{}.n_{}.clf".format(mode.lower(), kind, n_trees))
-
-
-    plt.figure(figsize=(8, 6))
-    plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (AUC = {:.2f})'.format(roc_auc))
-    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver Operating Characteristic (ROC)')
-    plt.legend(loc='lower right')
-    plt.show()
-
 
 if __name__ == '__main__':
     main()
